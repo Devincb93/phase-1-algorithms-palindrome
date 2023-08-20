f4a7605fa932d179395ed559876f7089cc307a1d
Devin <devincb93@gmail.com>
2023-08-20 15:59:16 -0700

    palindrome lab

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverse(word)
+
+  if(word === reversedWord){
+    return true
+  } else {
+    return false
+  }
+}
+
+
 /* 
-  Add your pseudocode here
+  reverse the input string
+
+  if the input string is the same as the reversed input string
+  return true
+  else
+  return false
 */
 
 /*
-  Add written explanation of your solution here
+  Wrote a function seperately in global to split, reverse, and then rejoin the word.
+  After creating that function that word is able to be put into the isPalindrome function and run as normal. 
+  The returned function should return true or false based off if the word is a palindrome or not.
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,26 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 }
 
 module.exports = isPalindrome;
